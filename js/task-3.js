233478ac275609d6b2900fc67e40d4c2e7bd48d4
Maksym Drachenko <maks7d@gmail.com>
2025-01-18 16:08:18 +0500

    task-for -3-task

--- js/task-3.js
+++ js/task-3.js
@@ -2,3 +2,110 @@
 
 // ? Завдання 3.1
 // ? Створи масив із 5 чисел і обчисли їхню суму.
+
+function sortArray(array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+}
+
+console.log(sortArray([5, 7, 10, 2, 24]));
+
+// ? Завдання 3.2
+// ? Напиши функцію, яка приймає масив і повертає його останній елемент.
+
+function lastElement(array) {
+  return array[array.length - 1];
+}
+
+console.log(lastElement(['HTML', 'CSS', 'Java', 'Javascript']));
+
+// ? Завдання 3.3
+// ? Використовуй метод slice() для створення нового масиву, який містить перші три елементи масиву [1, 2, 3, 4, 5].
+
+const firstArray = [1, 2, 3, 4, 5];
+const newArray = firstArray.slice(0, 3);
+console.log(newArray);
+
+// ? Завдання 3.4
+// ? Створи функцію, яка приймає масив чисел і повертає новий масив, де всі числа помножені на 2.
+
+function multiplicationArray(array1) {
+  let element = [];
+  for (const element1 of array1) {
+    element.push(element1 * 2);
+  }
+  return element;
+}
+
+console.log(multiplicationArray([3, 2, 6, 4, 5]));
+
+// ? Завдання 3.5
+// ? Напиши цикл for...of, який виводить всі елементи масиву у консоль.
+
+const array2 = [3, 2, 6, 4, 5];
+
+for (const item of array2) {
+  console.log(item);
+}
+
+// ? Завдання 3.6
+// ? Додай новий елемент у масив на початок і виведи результат.
+
+const array3 = [3, 4, 5];
+array3.unshift(1, 2);
+
+console.log(array3);
+
+// ? Завдання 3.7
+// ? Реалізуй функцію, яка об'єднує два масиви в один за допомогою методу concat().
+
+function twoArrow(firstArray1, secondArray1) {
+  return firstArray1.concat(secondArray1);
+}
+
+console.log(twoArrow([1, 2, 3], [4, 5, 6]));
+
+// ? Завдання 3.8
+// ? Визнач, чи є число 7 у масиві [1, 2, 3, 7, 8, 9], використовуючи метод includes().
+
+// function arrayIncludes(array4) {
+//   if (array4.includes(7)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(arrayIncludes([1, 2, 3, 7, 8, 9]));
+
+const array4 = [1, 2, 3, 7, 8, 9];
+// const incl = array4.includes(7) ? true : false;
+// console.log(incl);
+
+console.log(array4.includes(7));
+// console.log(array4.includes(7) ? true : false);
+
+// ? Завдання 3.9
+// ? Створи масив рядків і перетвори його у один рядок через кому за допомогою join().
+
+const array5 = ['HTML', 'CSS', 'Phyton', 'Java'];
+const newString = array5.join(', ');
+console.log(newString);
+// console.log(array5.join(', '));
+
+// ? Завдання 3.10
+// ? Реалізуй функцію, яка приймає масив і повертає кількість елементів, більших за 10.
+
+function arrayNumbers(numbers) {
+  let newArrayNumbers = [];
+  for (const number of numbers) {
+    if (number >= 10) {
+      newArrayNumbers.push(number);
+    }
+  }
+  return newArrayNumbers.length;
+}
+
+console.log(arrayNumbers([5, 8, 11, 25, 3, 67]));
---
+++ js/task-for-3-task.js
@@ -0,0 +1,144 @@
+// * Задачі на всі модулі (змішані)
+
+// ? Завдання 4.1
+// ? Напиши функцію, яка приймає масив чисел і повертає масив парних чисел.
+
+function arrayNumbers(numbers) {
+  let newNumbers = [];
+  for (const number of numbers) {
+    if (number % 2 === 0 && number !== 0) {
+      newNumbers.push(number);
+    }
+  }
+  return newNumbers;
+}
+
+console.log(arrayNumbers([13, 2, 0, 46, 7, 8]));
+
+// ? Завдання 4.2
+// ? Створи функцію, яка рахує кількість голосних літер у рядку.
+
+function letter(str) {
+  let totalVolwe = 0;
+  const strToLowerCase = str.toLowerCase();
+  const volwe = ['a', 'e', 'i', 'o', 'u'];
+  for (const item of strToLowerCase) {
+    if (volwe.includes(item)) {
+      totalVolwe += 1;
+    }
+  }
+  return totalVolwe;
+}
+
+console.log(letter('Hello world'));
+
+// ? Завдання 4.3
+// ? Реалізуй функцію, яка приймає число і повертає його факторіал.
+
+function factorials(oneNumber) {
+  let totalFactorials = 1;
+  for (let i = 1; i <= oneNumber; i++) {
+    totalFactorials *= i;
+  }
+  return totalFactorials;
+}
+
+console.log(factorials(5));
+
+// ? Завдання 4.4
+// ? Створи функцію, яка обчислює площу круга за заданим радіусом.
+
+function areaСircle(radiuce) {
+  return 3.14 * radiuce ** 2;
+}
+console.log(areaСircle(3));
+
+// ? Завдання 4.5
+// ? Напиши функцію, яка приймає два рядки і перевіряє, чи є один з них анаграмою іншого.
+
+function auditStr(firstStr, secondStr) {
+  let auditTotal = 0;
+  for (const item of firstStr) {
+    if (secondStr.includes(item)) {
+      auditTotal += 1;
+    }
+  }
+  if (auditTotal === secondStr.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(auditStr('qwert', 'tqwrea'));
+
+// ? Завдання 4.6
+// ? Визнач, чи є всі числа у масиві [2, 4, 6, 8] парними.
+
+const evenArray = [2, 4, 6, 8];
+function evenNumber(evenArray) {
+  let evenTotal = 0;
+  for (const even of evenArray) {
+    if (even % 2 === 0) {
+      evenTotal += 1;
+    }
+  }
+  return evenTotal === evenArray.length ? true : false;
+}
+
+console.log(evenNumber(evenArray));
+
+// ? Завдання 4.7
+// ? Напиши функцію, яка повертає масив чисел від 1 до N.
+
+function arrayNumbersN(N) {
+  let newArrayN = [];
+  for (let i = 1; i <= N; i++) {
+    newArrayN.push(i);
+  }
+  return newArrayN;
+}
+
+console.log(arrayNumbersN(5));
+
+// ? Завдання 4.8
+// ? Реалізуй функцію, яка повертає новий масив із унікальними елементами масиву [1, 2, 2, 3, 4, 4, 5].
+
+function uniqueArray(array1) {
+  let newUniqueArra = [];
+  for (const element of array1) {
+    if (!newUniqueArra.includes(element)) {
+      newUniqueArra.push(element);
+    }
+  }
+  return newUniqueArra;
+}
+
+console.log(uniqueArray([1, 2, 2, 3, 4, 4, 5]));
+
+// ? Завдання 4.9
+// ? Створи функцію, яка повертає найбільше число у масиві [10, 20, 5, 7, 100].
+
+function bigNumber(numbersBig) {
+  let maxNumber = numbersBig[0];
+  for (let i = 1; i < numbersBig.length; i++) {
+    if (numbersBig[i] > maxNumber) {
+      maxNumber = numbersBig[i];
+    }
+  }
+  return maxNumber;
+}
+console.log(bigNumber([10, 20, 5, 7, 100]));
+
+// ? Завдання 4.10
+// ? Реалізуй функцію, яка обчислює середнє арифметичне масиву чисел.
+
+function average(array2) {
+  let avaregeTotal = 0;
+  for (const element3 of array2) {
+    avaregeTotal += element3;
+  }
+  return avaregeTotal / array2.length;
+}
+
+console.log(average([1, 2, 3, 4, 15]));
